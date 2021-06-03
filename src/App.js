a53772a0b29c7f1eabbbd7d74fc4a865b7d16711
facilitador-pygma <85202215+facilitador-pygma@users.noreply.github.com>
2021-06-03 10:00:20 -0500

    Base proyecto
    Estructura base para empezar el proyecto

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+/**************************************/
+/*********** Importaciones ************/
+/**************************************/
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+//Propios de React
+import React from 'react';
+
+const App = () =>{
+  return(
+    <>
+    
+      <h2>Hola mundo</h2>
+
+    </>
   );
 }
+
 
 export default App;
